b46249f25ae21e11b6a4be6465480cfca5dd7608
Hirdyansh <hirdyanshsaxena7@gmail.com>
2026-09-11 23:24:39 +0530

    health routes

--- services/auth/src/app.js
+++ services/auth/src/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const authRoutes = require("./routes/auth.routes");
+const healthRoutes = require("./routes/health.routes");
+const { he } = require("zod/v4/locales");
 
 const app = express();
 
 app.use(express.json());
+app.use("/", healthRoutes);
 
 app.use("/api/auth", authRoutes);
 
